8639908893376f56644bf02b1ed5d1ec42e042d1
babbincocks <zabbybab@gmail.com>
2018-08-02 20:15:41 -0400

    Pretty sure I'm finished.

--- js_Mortgage/script.js
+++ js_Mortgage/script.js
@@ -1,5 +1,6 @@
 function calculateMonthly(loanAmount, loanLength, interestRate)
 {
+    
     let rate = interestRate / 1200;
     let monthly = 0;
     let inRate = rate + 1;
@@ -10,6 +11,60 @@ function calculateMonthly(loanAmount, loanLength, interestRate)
     inRate = Math.pow(inRate, loanLength);
     monthly = (loanAmount * ((rate * inRate)) / (inRate - 1));
 
+    return monthly;
     
 
 }
+
+
+function preCheck()
+    {
+        let loan = parseFloat(document.getElementById('txtPrincipal').value);
+        let loanLength = parseInt(document.getElementById('txtLength').value);
+        let rate = parseInt(document.getElementById('txtInterest').value);
+
+        let errLoan = document.getElementById("loanWarn");
+        let errLength = document.getElementById("timeWarn");
+        let errRate = document.getElementById("intWarn");
+
+        let clear = true;
+
+        errLoan.innerText = "";
+        errLength.innerText = "";
+        errRate.innerText = "";
+
+        if(loan == 0 || loan == NaN || loan == "")
+        {
+            
+            let loanNode = document.createTextNode("Needs a value");
+
+            errLoan.appendChild(loanNode);
+            clear = false;
+        }
+        
+        if(loanLength == 0 || loanLength == NaN || loanLength == "")
+        {
+            let lengthNode = document.createTextNode("Needs a value");
+
+            errLength.appendChild(lengthNode);
+            clear = false;
+        }
+            
+            if(rate == 0 || rate == NaN || rate == "")
+            {
+                let rateNode = document.createTextNode("Needs a value");
+
+                errRate.appendChild(rateNode);
+                clear = false;
+            }
+            if(clear == true)
+            {
+                var total = calculateMonthly(loan, loanLength, rate);
+                total = total.toFixed(2);
+                let result = document.getElementById('Result');
+                result.innerText = "Your monthly payment would be $" + total + "."
+
+            }
+            
+        
+    }
